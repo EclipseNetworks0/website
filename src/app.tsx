14c905b84f8632d0c9d99df96fa0c715512bb2cd
AsrayeDev <asrayeofficial@gmail.com>
2026-08-19 04:53:58 +1000

    feat: add meta tags for embeds
    Signed-off-by: AsrayeDev <asrayeofficial@gmail.com>

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import { MetaProvider } from "@solidjs/meta";
+import { MetaProvider, Title, Meta } from "@solidjs/meta";
 import Navbar from "~/components/Navbar";
 import Footer from "~/components/Footer";
 import "boxicons/css/boxicons.min.css";
@@ -10,6 +10,14 @@ import "./app.css";
 export default function App() {
   return (
     <MetaProvider>
+      <Title>Eclipse Networks</Title>
+      <Meta name="description" content="An Australian semi-serious FiveM roleplay server where community and enjoyment come first!" />
+      <Meta name="theme-color" content="#a100ed" />
+      <Meta property="og:site_name" content="Eclipse Networks" />
+      <Meta property="og:title" content="Eclipse Networks" />
+      <Meta property="og:description" content="An Australian semi-serious FiveM roleplay server where community and enjoyment come first!" />
+      <Meta property="og:type" content="website" />
+
       <Router
         root={(props) => (
           <div class="flex min-h-screen flex-col bg-[#111215]">
